9d9a2ee559a4b957b32acbd356a42bdf34c6b57f
neilmizzi <mizzin@hotmail.com>
2019-12-07 16:06:16 +0100

    initial commit

--- multi_armed_bandits.py
+++ multi_armed_bandits.py
@@ -1,15 +1,16 @@
-import random
-import numpy as np
-from scipy import stats
-
-'''
+"""
 if alpha = beta = 1 then we have uniform distribution
 if alpha = beta     then we have a symmetric distribution, where x = 1/2
 if alpha > beta     then the density is right-leaning (concentrated in the neighbourhood of 1)
                     the mean and variance will be able to be computed explicitly
-'''
+"""
 
 
 def thompson_rule_update(alpha: int = 1, beta: int = 1) -> int:
+
+    pass
+
+
+def k_armed_bandit(k: int = 8) -> [float]:
 
     pass
